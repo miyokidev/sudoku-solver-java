720334caa04c0171090e84338d7ab3d339d5e7f1
Brian Grin <bgrin@miyoki.ch>
2020-10-20 21:16:55 +0200

    Ajout du projet eclipse

---
+++ Sudoku Solver/src/SudokuSolver.java
@@ -0,0 +1,27 @@
+public class SudokuSolver {
+	public int[][] grid;
+	
+	public SudokuSolver(int[][] grid) {
+		this.grid = grid;
+	}
+	
+	private boolean checkRow() {
+		
+		return false;
+	}
+	
+	private boolean checkCol() {
+		
+		return false;
+	}
+	
+	private boolean checkBox() {
+		
+		return false;
+	}
+	
+	public int[][] solve() {
+		return null;
+	}
+	
+}
